65c9f65c41060186e6c5e3b42fe83d9b94c8a743
Spiegel <spiegel.im.spiegel@gmail.com>
2015-11-04 11:07:28 +0900

    auto commit test in 2015-11-04T11:07:28+09:00

--- commit-push.lua
+++ commit-push.lua
@@ -1,20 +1,17 @@
 require("hugolib")
 
-nyagos.write("\ngit add...\n")
 local errorlevel, errormessage = hugolib.git_add_all()
 if errorlevel ~= 0 then
 	nyagos.writerr("Error Message: "..errormessage.."\n")
 	os.exit(errorlevel)
 end
 
-nyagos.write("\ngit commit...\n")
 errorlevel, errormessage = hugolib.git_commit("auto commit test in "..os.date("%Y-%m-%dT%H:%M:%S+09:00"))
 if errorlevel ~= 0 then
 	nyagos.writerr("Error Message: "..errormessage.."\n")
 	os.exit(errorlevel)
 end
 
-nyagos.write("\ngit push...\n")
 errorlevel, errormessage = hugolib.git_push("origin", "master")
 if errorlevel ~= 0 then
 	nyagos.writerr("Error Message: "..errormessage.."\n")
