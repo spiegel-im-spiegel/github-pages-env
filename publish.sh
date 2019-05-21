c04ec9df4df1d08af1d60b025717bb3e1fdba690
Spiegel <spiegel.im.spiegel@gmail.com>
2019-05-21 21:21:02 +0900

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ./build.sh || exit 1
 pushd ../published
+comment="Auto commit in $(date -u '+%FT%T+00:00')"
+if [ $# -ne 0 ]; then
+  comment=$1
+fi
 git add --all || exit 1
-git commit -v -m "Auto commit in $(date -u '+%FT%T+00:00')" || exit 1
+git commit -v -m "$comment" || exit 1
 git push -u origin master || exit 1
 popd
