246265cc6593d0189ca8cd5ee506b23e5548cdfb
Spiegel <spiegel.im.spiegel@gmail.com>
2020-05-11 20:54:54 +0900

    Update reviews

--- reviews/make-review.sh
+++ reviews/make-review.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-sleep 1;cat card-4333.html   | books-data review -t review-book.html -c 4333   -r 4 --review-date 2019-09-21 --image-url https://text.baldanders.info/images/aozora/card4333.svg  --pipe
+sleep 1;cat card-4333.html   | books-data review -t review-book.html -c 4333   -r 4 --review-date 2020-05-11 --image-url https://text.baldanders.info/images/aozora/card4333.svg  --pipe
 sleep 1;cat B00O2O7JEA.html | books-data review -t review-book.html -a B00O2O7JEA -r 4 --review-date 2020-05-11 --pipe
 sleep 1;cat B00FIWCVFM.html | books-data review -t review-book.html -a B00FIWCVFM -r 4 --review-date 2020-05-11 --pipe
+sleep 1;cat ./done/B00NAQA33A.html | books-data review -t review-book.html -a B00NAQA33A -r 5 --review-date 2013-09-21 --pipe
 # ---
 # sleep 1;cat ./done/B07BKT4RX5.html | books-data review -t review-book.html -a B07BKT4RX5 -r 5 --review-date 2020-04-05 --pipe
 # sleep 1;cat ./done/B014VZ4FLE.html | books-data review -t review-book.html -a B014VZ4FLE -r 5 --review-date 2020-04-05 --pipe
@@ -129,7 +130,6 @@ sleep 1;cat B00FIWCVFM.html | books-data review -t review-book.html -a B00FIWCVF
 # sleep 1;cat ./done/B00I6IT1U8.html | books-data review -t review-book.html -a B00I6IT1U8 -r 4 --review-date 2015-04-02 --pipe
 # sleep 1;cat ./done/4480091599.html | books-data review -t review-book.html -a 4480091599 -r 4 --review-date 2019-04-14 --pipe
 # sleep 1;cat ./done/B01EL08HVS.html | books-data review -t review-book.html -a B01EL08HVS -r 5 --review-date 2016-07-23 --pipe
-# sleep 1;cat ./done/B00NAQA33A.html | books-data review -t review-book.html -a B00NAQA33A -r 5 --review-date 2018-12-21 --pipe
 # sleep 1;cat ./done/4835449517.html | books-data review -t review-book.html -a 4835449517 -r 5 --review-date 2014-12-01 --pipe
 # sleep 1;cat ./done/B00005HFJQ.html | books-data review -t review-book.html -a B00005HFJQ -r 5 --review-date 2019-06-05 --pipe
 # sleep 1;cat ./done/4560084122.html | books-data review -t review-book.html -a 4560084122 -r 4 --review-date 2019-06-05 --pipe
