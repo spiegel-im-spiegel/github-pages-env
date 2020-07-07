8451e536cffd9f892cc57759c6c15f8f000662cf
Spiegel <spiegel.im.spiegel@gmail.com>
2020-07-07 14:53:16 +0900

    New post (no-code)

--- reviews/make-review.sh
+++ reviews/make-review.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sleep 1;cat B000ALJ18G.html | books-data review -t review-book.html -a B000ALJ18G -r 0 --review-date 2020-07-07 --pipe
 sleep 1;cat B01GJZKCUC.html | books-data review -t review-book.html -a B01GJZKCUC -r 4 --review-date 2020-07-07 --pipe
 sleep 1;cat 4770501994.html | books-data review -t review-book.html -a 4770501994 -r 0 --review-date 2020-07-07 --pipe
 sleep 1;cat B07Y29NV9P.html | books-data review -t review-book.html -a B07Y29NV9P -r 0 --review-date 2020-07-07 --pipe
