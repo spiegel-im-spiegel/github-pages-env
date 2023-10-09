9f2e6f68b5aae837c60e6ae5faa4e1ed89706813
Spiegel <spiegel.im.spiegel@gmail.com>
2023-10-09 20:31:31 +0900

    New post (music)

--- reviews/make-review.sh
+++ reviews/make-review.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+sleep 1;cat B000064B01.html | books-data review -t review-book.html -a B000064B01 -r 4 --review-date 2023-10-09 --pipe
 sleep 1;cat B07TXY9KBJ.html | books-data review -t review-book.html -a B07TXY9KBJ -r 4 --review-date 2023-10-05 --pipe
 # ---
 # sleep 1;cat ./done/B00AAPY0ZQ.html | books-data review -t review-book.html -a B00AAPY0ZQ -r 0 --review-date 2023-09-18 --pipe
