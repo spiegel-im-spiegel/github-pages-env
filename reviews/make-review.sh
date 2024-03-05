075a9050953154e51a74275ae12e5ccea98399f2
Spiegel <spiegel.im.spiegel@gmail.com>
2024-03-05 19:56:22 +0900

    Update review

--- reviews/make-review.sh
+++ reviews/make-review.sh
@@ -2,8 +2,8 @@
 sleep 1;cat B09V3D51LF.html | books-data review -t review-book.html -a B09V3D51LF -r 5 --review-date 2024-03-05 --pipe
 sleep 1;cat B0CPM8V21D.html | books-data review -t review-book.html -a B0CPM8V21D -r 4 --review-date 2024-02-23 --pipe
 sleep 1;cat 4814400535.html | books-data review -t review-book.html -a 4814400535 -r 0 --review-date 2024-02-22 --pipe
+sleep 1;cat ./done/B0BL76SJL6.html | books-data review -t review-book.html -a B0BL76SJL6 -r 4 --review-date 2024-03-02 --pipe
 # ---
-# sleep 1;cat ./done/B0BL76SJL6.html | books-data review -t review-book.html -a B0BL76SJL6 -r 4 --review-date 2024-03-02 --pipe
 # sleep 1;cat ./done/B00CD1PTF0.html | books-data review -t review-book.html -a B00CD1PTF0 -r 5 --review-date 2024-03-02 --pipe
 # sleep 1;cat ./done/B00016AWEC.html | books-data review -t review-book.html -a B00016AWEC -r 5 --review-date 2024-03-02 --pipe
 # sleep 1;cat ./done/4000297244.html | books-data review -t review-book.html -a 4000297244 -r 4 --review-date 2024-02-23 --pipe
