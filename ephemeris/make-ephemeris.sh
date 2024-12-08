4f4836905418e1b01d5c869f3f68dc613f7e6c42
Spiegel <spiegel.im.spiegel@gmail.com>
2024-12-08 23:21:50 +0900

    New post (izumo)

--- ephemeris/make-ephemeris.sh
+++ ephemeris/make-ephemeris.sh
@@ -1,3 +1,4 @@
 #/bin/sh
-toolbox calendar lookup --start "2024-01-01" --end "2024-12-31" --holiday --template ./template.txt
-# toolbox calendar lookup --start "2024-01-01" --end "2024-12-31" --holiday --moon-phase --solar-term --eclipse --planet --template ./template.txt
+# toolbox calendar lookup --start "2025-01-01" --end "2025-12-31" --holiday --template ./template.txt
+toolbox calendar lookup --start "2025-01-01" --end "2025-12-31" --eclipse --template ./template.txt
+# toolbox calendar lookup --start "2025-01-01" --end "2025-12-31" --holiday --moon-phase --solar-term --eclipse --planet --template ./template.txt
--- reviews/make-review.sh
+++ reviews/make-review.sh
@@ -1,10 +1,13 @@
 #/bin/sh
-sleep 1;cat B0D9VNGMK3.html | books-data review -t review-book.html -a B0D9VNGMK3 -r 4 --review-date 2024-09-28 --pipe
-sleep 1;cat B0CD6L52NZ.html | books-data review -t review-book.html -a B0CD6L52NZ -r 4 --review-date 2024-11-23 --pipe
-sleep 1;cat B005SY3LUY.html | books-data review -t review-book.html -a B005SY3LUY -r 4 --review-date 2024-11-23 --pipe
-sleep 1;cat B00DFUFWUA.html | books-data review -t review-book.html -a B00DFUFWUA -r 4 --review-date 2017-03-05 --pipe
-sleep 1;cat 4886219454.html | books-data review -t review-book.html -a 4886219454 -r 4 --review-date 2024-11-26 --pipe
+sleep 1;cat B00IOGFPBG.html | books-data review -t review-book.html -a B00IOGFPBG -r 4 --review-date 2015-01-11 --pipe
+sleep 1;cat B0D95B4F97.html | books-data review -t review-book.html -a B0D95B4F97 -r 5 --review-date 2024-12-06 --pipe
+sleep 1;cat 4416723660.html | books-data review -t review-book.html -a 4416723660 -r 5 --review-date 2024-12-05 --pipe
 # ---
+# sleep 1;cat ./done/B0D9VNGMK3.html | books-data review -t review-book.html -a B0D9VNGMK3 -r 4 --review-date 2024-09-28 --pipe
+# sleep 1;cat ./done/B0CD6L52NZ.html | books-data review -t review-book.html -a B0CD6L52NZ -r 4 --review-date 2024-11-23 --pipe
+# sleep 1;cat ./done/B005SY3LUY.html | books-data review -t review-book.html -a B005SY3LUY -r 4 --review-date 2024-11-23 --pipe
+# sleep 1;cat ./done/B00DFUFWUA.html | books-data review -t review-book.html -a B00DFUFWUA -r 4 --review-date 2017-03-05 --pipe
+# sleep 1;cat ./done/4886219454.html | books-data review -t review-book.html -a 4886219454 -r 4 --review-date 2024-11-26 --pipe
 # sleep 1;cat ./done/4416723660.html | books-data review -t review-book.html -a 4416723660 -r 4 --review-date 2024-11-17 --pipe
 # sleep 1;cat ./done/B0DB51XXGF.html | books-data review -t review-book.html -a B0DB51XXGF -r 0 --review-date 2024-11-17 --pipe
 # sleep 1;cat ./done/B0DMVBX6WC.html | books-data review -t review-book.html -a B0DMVBX6WC -r 4 --review-date 2024-11-17 --pipe
