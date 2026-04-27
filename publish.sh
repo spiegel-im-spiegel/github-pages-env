bad73fcc8c8dad679bb5e494c19a2205456fd91c
Spiegel <spiegel.im.spiegel@gmail.com>
2026-04-27 10:11:33 +0900

    fix(script): resolve publish message lookup order

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ./build.sh || exit 1
-pushd ../text-publishd
 if [ $# -ne 0 ]; then
   comment=$1
 else
   comment="$(git log -1 --pretty=%s)"
 fi
+pushd ../text-publishd
 git add --all || exit 1
 git commit -v -m "$comment" || exit 1
 git push -u origin master || exit 1
