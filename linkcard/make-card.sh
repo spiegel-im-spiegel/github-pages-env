7362f722645b19cc1309084e8ed781815bd2fff0
Spiegel <spiegel.im.spiegel@gmail.com>
2026-07-08 19:41:42 +0900

    Add linkcards support: data, partial, shortcode and make-card script

---
+++ linkcard/make-card.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+linkcard --rating 0 https://text.baldanders.info/ -c ""
+linkcard --rating 3 https://www.subarusya.jp/book/b676548.html -c "米国では某企業から圧力がかかってると聞いて紙の本を買ってみた"
