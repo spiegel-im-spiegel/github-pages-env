6252592212393f0a49d383efb58e0046223097df
Spiegel <spiegel.im.spiegel@gmail.com>
2026-08-25 16:43:14 +0900

    Add reading report for A Relaxed Guide to the Classics

--- linkcard/make-card.sh
+++ linkcard/make-card.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-linkcard https://zen.ac.jp/press/books/b_0013 --rating 4 --release-date "2026-06-29" -w 150 -c "「ボブおじさん」による最新刊？ かな。2世紀以上に渡るコンピュータとプログラマの歴史と回顧録。気楽な読み物として楽しめる。デジタル化希望！"
-linkcard https://technical-book-reading.connpass.com/event/401737/ -w 110
-linkcard https://zen.ac.jp/press/books/b_0015 --rating 4 --release-date "2026-07-10" -w 150 -c "実務に即効性があるわけではないが，ものの「考え方」を示す本としてはよく出来ている。ソフトウェア技術史の読み物としても面白い。デジタル化希望！"
+linkcard https://www.takeshobo.co.jp/book/b10080622.html --rating 4 --release-date "2024-05-16" -w 110 -c "変態（笑）清少納言を中心としたコメディ平安史の最終巻。読んでるとこっちのほうが正史では？とか思ってしまう。"
+linkcard https://www.kadokawa.co.jp/product/322411000125/ --rating 4 --release-date "2025-07-25" -w 110 -c "にじさんじのライバー<a href ="https://www.youtube.com/@ShiorihaRuri">栞葉るり</a>さんによるエッセイ？ かな？ 平安時代のいわゆる女流文学と現代オタクとの対比がめっちゃ面白い。"
+#linkcard https://zen.ac.jp/press/books/b_0013 --rating 4 --release-date "2026-06-29" -w 150 -c "「ボブおじさん」による最新刊？ かな。2世紀以上に渡るコンピュータとプログラマの歴史と回顧録。気楽な読み物として楽しめる。デジタル化希望！"
+#linkcard https://technical-book-reading.connpass.com/event/401737/ -w 110
+#linkcard https://zen.ac.jp/press/books/b_0015 --rating 4 --release-date "2026-07-10" -w 150 -c "実務に即効性があるわけではないが，ものの「考え方」を示す本としてはよく出来ている。ソフトウェア技術史の読み物としても面白い。デジタル化希望！"
 #linkcard https://www.404media.co/xs-algorithm-feeds-off-ragebait-and-impacts-democrats-more-study-finds/ -w 150
 #linkcard https://www.404media.co/person-hides-prompt-injection-in-legal-filing-telling-ai-to-side-with-them/ -w 150
 #linkcard https://koyomi.online/koyomigonomi_s/ --rating 5 -w 150 -c "暦の擬人化手法も含めて面白い作品。感想は<a href='/remark/2026/02/the-calendar-as-astronomical-folklore/'>こちら</a>。"
